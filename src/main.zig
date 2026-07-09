f459961f095e9a8188e031ca44e44a1797bd2e53
Lucas Martins dos Santos <lukita.m.s@gmail.com>
2026-07-09 19:56:56 -0300

    implement ball collision detection with paddle

--- src/main.zig
+++ src/main.zig
@@ -30,6 +30,32 @@ const Ball = struct {
         const ball_to_paddle = paddle_middle.subtract(self.position);
         self.direction = ball_to_paddle.normalize();
     }
+
+    pub fn checkCollisionWithPaddle(self: *Ball, paddle_rect: *const rl.Rectangle) void {
+        if (!rl.checkCollisionCircleRec(self.position, Ball.RADIUS, paddle_rect.*)) return;
+        
+        var collision_normal: rl.Vector2 = .zero();
+
+        if (self.position.y < paddle_rect.y + paddle_rect.height) {
+            collision_normal = collision_normal.add(.init(0, -1));
+            self.position.y = paddle_rect.y - Ball.RADIUS;
+        }
+
+        if (self.position.y > paddle_rect.y + paddle_rect.height) {
+            collision_normal = collision_normal.add(.init(0, 1));
+            self.position.y = paddle_rect.y + paddle_rect.height + Ball.RADIUS;
+        }
+
+        if (self.position.x < paddle_rect.x) {
+            collision_normal = collision_normal.add(.init(-1, 0));
+        }
+
+        if (self.position.x > paddle_rect.x + paddle_rect.width) {
+            collision_normal = collision_normal.add(.init(1, 0));
+        }
+
+        self.position = reflect(self.position, collision_normal);
+    }
 };
 
 var started = false;
@@ -40,19 +66,24 @@ fn restart(paddle: *Paddle, ball: *Ball) void {
     started = false;
 }
 
+fn reflect(dir: rl.Vector2, normal: rl.Vector2) rl.Vector2 {
+    const new_dir: rl.Vector2 = dir.reflect(normal.normalize());
+    return new_dir.normalize();
+}
+
 pub fn main(_: std.process.Init) !void {
     rl.setConfigFlags(.{ .vsync_hint = true });
 
     rl.initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "breakout");
     defer rl.closeWindow();
-    
+
     rl.setTargetFPS(120);
 
     var paddle = Paddle{};
     var ball = Ball{};
 
     restart(&paddle, &ball);
-    
+
     while (!rl.windowShouldClose()) {
         var dt: f32 = 0;
         if (!started) {
@@ -82,21 +113,20 @@ pub fn main(_: std.process.Init) !void {
         paddle.position_x += paddle.move_velocity * dt;
         paddle.position_x = std.math.clamp(paddle.position_x, 0, SCREEN_SIZE - Paddle.WIDTH);
 
+        const paddle_rect = rl.Rectangle{ .x = paddle.position_x, .y = Paddle.POSITION_Y, .width = Paddle.WIDTH, .height = Paddle.HEIGHT };
+
+        ball.checkCollisionWithPaddle(&paddle_rect);
+
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.init(150, 190, 220, 255));
 
-        const camera = rl.Camera2D{ 
-            .offset = .zero(),
-            .rotation = 0,
-            .target = .zero(),
-            .zoom = @as(f32, @floatFromInt(rl.getScreenHeight())) / @as(f32, @floatFromInt(SCREEN_SIZE))
-        };
+        const camera_zoom = @as(f32, @floatFromInt(rl.getScreenHeight())) / @as(f32, @floatFromInt(SCREEN_SIZE));
+        const camera = rl.Camera2D{ .offset = .zero(), .rotation = 0, .target = .zero(), .zoom = camera_zoom };
         camera.begin();
         defer camera.end();
 
-        const paddle_rect = rl.Rectangle{ .x = paddle.position_x, .y = Paddle.POSITION_Y, .width = Paddle.WIDTH, .height = Paddle.HEIGHT };
         rl.drawRectangleRec(paddle_rect, Paddle.COLOR);
         rl.drawCircleV(ball.position, Ball.RADIUS, Ball.COLOR);
     }
